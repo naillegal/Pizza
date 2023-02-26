e671e24119880bd21ea788087c9926eff3e0460e
naillegal <quluzade-nail@mail.ru>
2023-02-27 00:36:16 +0400

    Pizza

--- main.js
+++ main.js
@@ -21,3 +21,43 @@ function sizeHandler(e) {
             break
     }
 }
+const chefbox = document.querySelector('.chefbox')
+const sauce = document.querySelector('.sauce')
+const saucebox = document.querySelector('.saucebox')
+sauce.onclick = function () {
+    saucebox.style.display = 'block'
+    cheesebox.style.display = 'none'
+    meatsbox.style.display = 'none'
+    veggiesbox.style.display = 'none'
+    chefbox.style.display = 'none'
+}
+
+const cheese = document.querySelector('.cheese')
+const cheesebox = document.querySelector('.cheesebox')
+cheese.onclick = function () {
+    saucebox.style.display = 'none'
+    cheesebox.style.display = 'block'
+    meatsbox.style.display = 'none'
+    veggiesbox.style.display = 'none'
+    chefbox.style.display = 'none'
+}
+
+const meats = document.querySelector('.meats')
+const meatsbox = document.querySelector('.meatsbox')
+meats.onclick = function () {
+    saucebox.style.display = 'none'
+    cheesebox.style.display = 'none'
+    meatsbox.style.display = 'block'
+    veggiesbox.style.display = 'none'
+    chefbox.style.display = 'none'
+}
+
+const veggies = document.querySelector('.veggies')
+const veggiesbox = document.querySelector('.veggiesbox')
+veggies.onclick = function () {
+    saucebox.style.display = 'none'
+    cheesebox.style.display = 'none'
+    meatsbox.style.display = 'none'
+    veggiesbox.style.display = 'block'
+    chefbox.style.display = 'none'
+}
